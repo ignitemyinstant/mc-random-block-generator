d7c27378790bd93026d257d93bb8f86182dc6d69
ignitemyinstant <itzjennyjenjen@gmail.com>
2026-05-21 22:14:29 +0800

    Create random_block_generator.py

---
+++ random_block_generator.py
@@ -0,0 +1,13 @@
+import random
+
+blocks = [
+    "Stone",
+    "Dirt",
+    "Grass Block",
+    "Sand",
+    "Oak Log",
+    "Cobblestone"
+]
+
+random_block = random.choice(blocks)
+print("Random Minecraft block:", random_block)
